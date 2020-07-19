4cf926dd73318fbf2bbf4ac35ec52778a184955b
wanvipar <68480880+wanvipar@users.noreply.github.com>
2020-07-20 00:56:00 +0700

    Add files via upload

---
+++ earn.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\n";
+	cout << "   1st Term of 2020\t\\6306021620123   Wanvipar Ruka \n"<<endl;
+	cout << "------------------------------------------------------------------------"<<endl;
+	cout << " |  Subject ID | Subject name             |Score (100) | Grade  |"<<endl;
+	cout << "------------------------------------------------------------------------"<<endl;
+	cout << " |  4570034    | Introduction to computer |    80      | \"A\"    |"<<endl;
+	cout << " |  4580247    | Englist                  |    74      | \"B\"    |"<<endl;
+	cout << " |  4570034    | Design Thinking          |    92      | \"A\"    |"<<endl;
+	cout << "------------------------------------------------------------------------"<<endl;
+	cout << " |  TOTAL      |Subject = 3               | Average = 3.87 |"<<endl;
+	cout << "==========================================\n\n"<<endl;
+	cout << "kmutnb\n"<<endl;
+	return (0);
+
+
+
+
+}
